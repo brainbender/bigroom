ff18e343b2f1e669f0f93c0332120a8cd2097f10
brainbender <brainbender.org@gmail.org>
2013-11-07 01:10:20 -0500

    base simple cms modified, add features tomorrow and display, css etc.

--- bigroomcms.php
+++ bigroomcms.php
@@ -1,5 +1,5 @@
 <?php
-//create class
+//base cms system for demo website, simple cms modified. 
 class bigroomCMS {
 //swap these for other server info when reusing
   var $host = 'localhost';
@@ -8,11 +8,11 @@ class bigroomCMS {
   var $table ='demo';
 
   public function display_public() {
-    $q = "SELECT * FROM testDB ORDER BY created DESC LIMIT 10";
-    $r = mysql_query($q);
+    $query = "SELECT * FROM demoDB ORDER BY created DESC LIMIT 10";
+    $m = mysql_query($query);
 
-    if ( $r !== false && mysql_num_rows($r) > 0 ) {
-      while ( $a = mysql_fetch_assoc($r) ) {
+    if ( $r !== false && mysql_num_rows($m) > 0 ) {
+      while ( $a = mysql_fetch_assoc($m) ) {
         $title = stripslashes($a['title']);
         $bodytext = stripslashes($a['bodytext']);
 
@@ -32,7 +32,7 @@ ENTRY_DISPLAY;
     } else {
       $entry_display = <<<ENTRY_DISPLAY
 
-    <h2> This Page Is Under Construction </h2>
+    <h2> Welcome </h2>
     <p>
       You have not posted anything yet. <BR>Please click the
       link below to add an entry!
@@ -43,7 +43,7 @@ ENTRY_DISPLAY;
     $entry_display .= <<<ADMIN_OPTION
 
     <p class="admin_link">
-      <a href="{$_SERVER['PHP_SELF']}?admin=1">Add a New Entry</a>
+      <a href="{$_SERVER['PHP_SELF']}?admin=1">Add a new entry.</a>
     </p>
 
 ADMIN_OPTION;
@@ -57,19 +57,19 @@ ADMIN_OPTION;
     <form action="{$_SERVER['PHP_SELF']}" method="post">
     
       <label for="title">Title:</label><br />
-      <input name="title" id="title" type="text" maxlength="150" />
+      <input name="title" id="title" type="text" maxlength="250" />
       <div class="clear"></div>
      
       <label for="bodytext">Body Text:</label><br />
       <textarea name="bodytext" id="bodytext"></textarea>
       <div class="clear"></div>
       
-      <input type="submit" value="Create This Entry!" />
+      <input type="submit" value="Submit this entry." />
     </form>
     
     <br />
     
-    <a href="display.php">Back to Home</a>
+    <a href="display.php">Home</a>
 
 ADMIN_FORM;
   }
@@ -80,8 +80,8 @@ ADMIN_FORM;
     if ( $_POST['bodytext'])
       $bodytext = mysql_real_escape_string($_POST['bodytext']);
     if ( $title && $bodytext ) {
-      $created = time();
-      $sql = "INSERT INTO testDB VALUES('$title','$bodytext','$created')";
+      $created = date('l dS F Y');;
+      $sql = "INSERT INTO demoDB VALUES('$title','$bodytext','$created')";
       return mysql_query($sql);
     } else {
       return false;
@@ -89,7 +89,7 @@ ADMIN_FORM;
   }
 
   public function connect() {
-    mysql_connect($this->host,$this->username,$this->password) or die("Could not connect. " . mysql_error());
+    mysql_connect($this->host,$this->username,$this->password) or die("Connection or authentication failure." . mysql_error());
     mysql_select_db($this->table) or die("Could not select database. " . mysql_error());
 
     return $this->buildDB();
@@ -97,10 +97,10 @@ ADMIN_FORM;
 
   private function buildDB() {
     $sql = <<<MySQL_QUERY
-CREATE TABLE IF NOT EXISTS testDB (
-title		VARCHAR(150),
+CREATE TABLE IF NOT EXISTS demoDB (
+title		VARCHAR(50),
 bodytext	TEXT,
-created		VARCHAR(100)
+created		VARCHAR(50)
 )
 MySQL_QUERY;
 
